c401a43809c85021ab90da5bebb56f27ea1206f4
tak458 <takah458@gmail.com>
2017-01-27 18:37:47 +0900

    スクリプトを修正

--- scripts/CreatePage.ps1
+++ scripts/CreatePage.ps1
@@ -1,6 +1,14 @@
+# scriptsディレクトリの一つ上のディレクトリに移動
+$previousPath = Get-Location;
+$path = Split-Path -Parent $MyInvocation.MyCommand.Path;
+Set-Location ($path + "\..\");
+
+# ファイル作成
 git pull;
-$FileName = ((get-date -Format "yyyy\\MM\\dd")+".md");
+$FileName = ((get-date -Format "yyyy\\MM\\dd") + ".md");
 if(-NOT (Test-Path $FileName)){
     New-Item $FileName;
-    git add $FileName;
 }
+
+# 元の場所に戻す
+Set-Location $previousPath;
