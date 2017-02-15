c4ef6f2b646e2cf007b8773db659ce2602c79c17
tak458 <takah458@gmail.com>
2017-02-15 20:00:03 +0900

    commit at the end of today

--- scripts/CommitAtEndOfDay.ps1
+++ scripts/CommitAtEndOfDay.ps1
@@ -1,5 +1,5 @@
 # 空のファイルを削除
-get-childitem -recurse -include *.md -exclude readme.md|?{$_.length -eq 0}|%{remove-item $_}
+Get-ChildItem -Recurse -Include *.md -Exclude readme.md | Where-Object { $_.Length -eq 0 } | ForEach-Object { Remove-Item $_ }
 
 git add -A
 git commit -m "commit at the end of today"
