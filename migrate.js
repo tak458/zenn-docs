1f1605c8678c8a842241814623f4d32b52e4af3d
tak458 <takah458@gmail.com>
2021-03-21 01:27:10 +0900

    リファクタ

--- migrate.js
+++ migrate.js
@@ -5,9 +5,11 @@ const https = require("https");
 const crypto = require("crypto");
 const fs = require("fs").promises;
 
-const username = process.env.USER_NAME;
-
-async function getQiita() {
+/**
+ * Qiita API から記事を取得する
+ * @returns
+ */
+async function getQiita(username) {
   return new Promise((resolve, reject) => {
     const req = https.request(
       `https://qiita.com/api/v2/users/${username}/items`,
@@ -29,23 +31,38 @@ async function getQiita() {
   });
 }
 
-// refs https://github.com/zenn-dev/zenn-editor/blob/02bf9b1e245f0b187470f4cdbe80f5fe38656829/packages/zenn-cli/cli/new-article.ts#L13-L17
+/**
+ * アイキャッチの絵文字を生成する
+ * refs https://github.com/zenn-dev/zenn-editor/blob/02bf9b1e245f0b187470f4cdbe80f5fe38656829/packages/zenn-cli/cli/new-article.ts#L13-L17
+ * @param {string} src
+ * @returns
+ */
 function pickRandomEmoji(src) {
   // prettier-ignore
-  const emojiList =["😺","📘","📚","📑","😊","😎","👻","🤖","😸","😽","💨","💬","💭","👋", "👌","👏","🙌","🙆","🐕","🐈","🦁","🐷","🦔","🐥","🐡","🐙","🍣","🕌","🌟","🔥","🌊","🎃","✨","🎉","⛳","🔖","📝","🗂","📌"]
+  const emojiList = ["😺","📘","📚","📑","😊","😎","👻","🤖","😸","😽","💨","💬","💭","👋","👌","👏","🙌","🙆","🐕","🐈","🦁","🐷","🦔","🐥","🐡","🐙","🍣","🕌","🌟","🔥","🌊","🎃","✨","🎉","⛳","🔖","📝","🗂","📌"]
+  // src 文字列からランダムな絵文字を抽出
   const bytes = Array.from(new TextEncoder().encode(src));
   const bytesSum = bytes.reduce(
     (pre, cur) => (pre + cur) % emojiList.length,
     0
   );
-  return emojiList[Math.floor(bytesSum)];
+  return emojiList[bytesSum];
 }
 
+/**
+ * zenn slug id を生成する
+ * @param {string} src
+ * @returns
+ */
 function pickRandomSlug(src) {
   const hash = crypto.createHash("md5").update(src).digest("hex");
   return hash.substring(0, 20);
 }
 
+/**
+ * Qiita 記事から zenn 記事を生成する
+ * @param {*} qiitaArticle
+ */
 async function createZennArticle(qiitaArticle) {
   const topics = qiitaArticle.tags.map((tag) => tag.name);
   const filename = `./articles/${pickRandomSlug(qiitaArticle.body)}.md`;
@@ -63,7 +80,10 @@ updated_at: "${qiitaArticle.updated_at}"
 }
 
 async function main() {
-  const articles = JSON.parse(await getQiita()).map((article) => article);
+
+  const username = process.env.USER_NAME;
+
+  const articles = JSON.parse(await getQiita(username));
 
   await Promise.all(articles.map(createZennArticle));
 }
